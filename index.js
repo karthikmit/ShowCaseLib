d5fc5667f8edc08a44d8d4242f84d9e905421ed6
Karthikeyan Subbaraj <Karthikeyan.Subbaraj@go-mmt.com>
2017-08-28 11:01:22 +0530

    Added bridge method show in index JS File.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import { NativeModules } from 'react-native';
 
 const { RNShowCaseLib } = NativeModules;
-export default RNShowCaseLib;
+
+// Return an object which contains essential APIs to access the native module.
+export default {
+
+    show: (options) => {
+        return new Promise((resolve, reject) => {
+            RNShowCaseLib.show(options, resolve, reject);
+        });
+    }
+};
